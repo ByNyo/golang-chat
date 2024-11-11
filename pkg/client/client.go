c7914b226160771af126f0e99cec78b38de16392
Leon Zuelsdorf <leonzuelsdorf@gmail.com>
2024-11-11 15:35:19 +0100

    feat(client): Created client.go

---
+++ pkg/client/client.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"golang.org/x/net/websocket"
+	"os"
+	"strings"
+	"time"
+)
+
+var cmdPrefix = "%"
+
+func main() {
+	buf := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter your Username: ")
+	username, err := buf.ReadString('\n')
+	if err != nil {
+		fmt.Println(err)
+	}
+	username = strings.TrimSpace(username)
+	ws, err := Connect(username)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	ListenForMessages(ws, buf, username)
+}
+
+func ListenForMessages(ws *websocket.Conn, buf *bufio.Reader, username string) {
+	defer ws.Close()
+	go func() {
+		fmt.Print("> ")
+		for {
+			message, err := buf.ReadString('\n')
+			if err != nil {
+				fmt.Println(err)
+			}
+			if message == cmdPrefix+"exit\n" {
+				os.Exit(0)
+			}
+			msg := fmt.Sprintf("\r%s: %s", username, string(message))
+			ws.Write([]byte(msg))
+			time.Sleep(100 * time.Millisecond)
+		}
+	}()
+	for {
+		var msg = make([]byte, 1024)
+		n, err := ws.Read(msg)
+		if err != nil {
+			return
+		}
+		message := string(msg[:n])
+		fmt.Print(message)
+		fmt.Print("> ")
+	}
+}
+
+func Connect(username string) (conn *websocket.Conn, err error) {
+	ws, err := websocket.Dial("ws://localhost:8080/ws?username="+username, "", "http://localhost/")
+	if err != nil {
+		return nil, err
+	}
+	return ws, nil
+}
