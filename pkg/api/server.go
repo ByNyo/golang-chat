bb0eec00ed050b501964718c7f88e18e3e7aa8bf
Leon Zuelsdorf <leonzuelsdorf@gmail.com>
2024-11-11 15:34:48 +0100

    feat(server): Created server.go

---
+++ pkg/api/server.go
@@ -0,0 +1,53 @@
+package api
+
+import (
+	"fmt"
+	"golang.org/x/net/websocket"
+	"io"
+	"sync"
+)
+
+type Server interface {
+	HandleWS(ws *websocket.Conn)
+}
+
+type server struct {
+	conns map[*websocket.Conn]bool
+	mu    sync.Mutex
+}
+
+func NewServer() Server {
+	return &server{conns: make(map[*websocket.Conn]bool)}
+}
+
+func (s *server) HandleWS(ws *websocket.Conn) {
+	fmt.Println("incoming connection from client:", ws.RemoteAddr())
+	s.conns[ws] = true
+	fmt.Println(s.conns)
+	s.readLoop(ws)
+}
+
+func (s *server) broadcast(b []byte) {
+	for ws := range s.conns {
+		go func(ws *websocket.Conn) {
+			if _, err := ws.Write(b); err != nil {
+				fmt.Println("error writing to websocket:", err)
+			}
+		}(ws)
+	}
+}
+
+func (s *server) readLoop(ws *websocket.Conn) {
+	buf := make([]byte, 1024)
+	for {
+		n, err := ws.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Println("read error:", err)
+			continue
+		}
+		s.broadcast(buf[:n])
+	}
+}
